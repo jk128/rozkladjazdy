6b13398d97e69b6031f399d867df048f73c0c7ab
tomekp27 <t.plesniak1@gmail.com>
2014-10-28 20:21:23 +0100

    Initial commit

---
+++ RozkladJazdy/src/com/example/rozkladjazdy/FavoriteFragment.java
@@ -0,0 +1,21 @@
+package com.example.rozkladjazdy;
+
+import com.example.rozkladjazdy.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class FavoriteFragment extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_favorite, container, false);
+         
+        return rootView;
+   
+}
+}
---
+++ RozkladJazdy/src/com/example/tabsswipe/adapter/TabsPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.tabsswipe.adapter;
+
+import com.example.rozkladjazdy.*;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+ 
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+ 
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+ 
+    @Override
+    public Fragment getItem(int index) {
+ 
+        switch (index) {
+        case 0:
+            // Top Rated fragment activity
+            return new FavoriteFragment();
+        case 1:
+            // Games fragment activity
+            return new MenuFragment();
+        case 2:
+            // Movies fragment activity
+            return new SearchFragment();
+        }
+ 
+        return null;
+    }
+ 
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 3;
+    }
+ 
+}
